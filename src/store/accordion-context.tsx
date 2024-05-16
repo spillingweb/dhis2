b8d2728ffd6bed4de319a68d6fdbe319179d8e58
spillingweb <khspilling@gmail.com>
2024-05-16 10:12:41 +0200

    fixed Typescript error

--- src/store/accordion-context.tsx
+++ src/store/accordion-context.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext, ReactNode } from "react";
+import { useState, createContext, useContext } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 type AccordionContextType = {
